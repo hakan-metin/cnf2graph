2bd9fc4297eccf903c94ac1cfcdd5c3ff2cc57e5
Hakan Metin <hakan.metin@lip6.fr>
2018-01-23 18:36:43 +0100

    Start to implement graph

---
+++ cnf2graph/Graph.h
@@ -0,0 +1,23 @@
+
+// Copyright 2017 Hakan Metin
+
+
+#ifndef INCLUDE_CNF2GRAPH_GRAPH_H_
+#define INCLUDE_CNF2GRAPH_GRAPH_H_
+
+namespace cnf2graph {
+
+class Graph {
+ public:
+    Graph();
+    ~Graph();
+};
+
+
+class AdjacencyGraph : public Graph {
+ public:
+};
+
+}  // namespace cnf2graph
+
+#endif  // INCLUDE_CNF2GRAPH_GRAPH_H_
